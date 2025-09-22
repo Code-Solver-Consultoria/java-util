a9a5af5735d1c1802d5c43d3031ee6a05558894a
LVRodrigues <luciano@codesolver.com.br>
2025-09-22 20:51:03 +0000

    Site de documentação no padrão da Code Solver.

--- src/main/java/br/com/codesolver/digester/AlgorithmType.java
+++ src/main/java/br/com/codesolver/digester/AlgorithmType.java
@@ -16,12 +16,26 @@ public enum AlgorithmType {
 	MD5("MD5"),
 	/** Hash SHA-1. */
 	SHA_1("SHA-1"),
+	/** Hash SHA-224. */
+	SHA_224("SHA-224"),
 	/** Hash SHA-256. */
 	SHA_256("SHA-256"),
 	/** Hash SHA-384. */
 	SHA_384("SHA-384"),
 	/** Hash SHA-512. */
-	SHA_512("SHA-512");
+	SHA_512("SHA-512"),
+	/** Hash SHA3-224. */
+	SHA3_224("SHA3-224"),
+	/** Hash SHA3-256. */
+	SHA3_256("SHA3-256"),
+	/** Hash SHA3-384. */
+	SHA3_384("SHA3-384"),
+	/** Hash SHA3-512. */
+	SHA3_512("SHA3-512"),
+	/** Hash SHAKE128-256. */
+	SHAKE128_256("SHAKE128-256"),
+	/** Hash SHAKE256-512. */
+	SHAKE256_512("SHAKE256-512");
 	
 	/** Nome do algorítimo. */
 	private String value;
